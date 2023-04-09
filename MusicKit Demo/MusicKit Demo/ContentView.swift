bbbab31d17ca9cc42f75037e9e6824f6b94501e9
Michael Parekh <mparekh4@illinois.edu>
2023-04-09 14:56:06 -0400

    build req framework

--- MusicKit Demo/MusicKit Demo/ContentView.swift
+++ MusicKit Demo/MusicKit Demo/ContentView.swift
@@ -32,8 +32,36 @@ struct ContentView: View {
         }
     }
     
+    // Use a private anonymous closure to hold the request (fetch items from the Apple Music catalog using a search term).
+    private let request: MusicCatalogSearchRequest = {
+        var request = MusicCatalogSearchRequest(term: "Happy", types: [Song.self])
+        request.limit = 25
+        return request
+    }()
+    
     private func fetchMusic() {
-        
+        Task {
+            // Request permissions to leverage Apple Music.
+            let status = await MusicAuthorization.request()
+            
+            switch status {
+            case .authorized:
+                // Make a request => get response containing songs.
+                do {
+                    let result = try await request.response()
+                    // Compact map the result songs into the declared 'Item' struct (since we are updating the 'songs' state, the view should update automatically).
+                    self.songs = result.songs.compactMap({
+                        return .init(name: $0.title,
+                                     artist: $0.artistName,
+                                     imageUrl: $0.artwork?.url(width: 75, height: 75))
+                    })
+                } catch {
+                    print(String(describing: error))
+                }
+            default:
+                break
+            }
+        }
     }
 }
 
